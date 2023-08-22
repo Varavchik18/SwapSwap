802adf6276a6d62a36eb60f481d464a6387a89e1
varavchik18 <pashavarava1806@gmail.com>
2023-08-22 17:44:22 +0300

    Clean architecture backend

---
+++ api/Application/RegisterService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application
+{
+    public static class RegisterService
+    {
+        public static void ConfigureApplication(this IServiceCollection services,
+        IConfiguration configuration)
+        {
+
+        }
+    }
+}
---
+++ api/Infrastructure/RegisterService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure
+{
+    public static class RegisterService
+    {
+        public static void ConfigureInfraStructure(this IServiceCollection services,
+        IConfiguration configuration)
+        {
+
+        }
+    }
+}
