29345b64d62e516e94f00cd81ec67417855e725b
varavchik18 <pashavarava1806@gmail.com>
2023-08-23 11:25:00 +0300

    Strange cash commit. Test gitignore.

---
+++ api/Domain/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Models
+{
+    public class Category
+    {
+        public int IdCategory { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
--- api/Domain/Models/Product.cs
+++ api/Domain/Models/Product.cs
@@ -2,5 +2,13 @@
 {
     public class Product
     {
+        public int IdProduct { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public string City { get; set; }
+        public string? Image { get; set; }
+        public int IdCategory { get; set; }
     }
 }
